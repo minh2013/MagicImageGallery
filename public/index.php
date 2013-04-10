d0d8608a41109bccb2bf13d3425a6d68efd5f5df
Nguyen Quang Minh Hoang <minh@pictura.com.au>
2013-04-10 23:09:00 +1000

    Add skeleton framework and code HTML upload page

--- public/index.php
+++ public/index.php
@@ -1,2 +1,21 @@
-<?php
-	echo "Hello world!";
+<?php include('tpl/header.php'); ?>
+
+<!-- Primary Page Layout
+================================================== -->
+
+<!-- Delete everything in this .container and get started on your own site! -->
+
+<div class="container content">
+	<div class="sixteen columns">
+        <h2>Upload an image</h2>
+        <form action="upload_file.php" method="post" enctype="multipart/form-data" class="upload-image">
+            <label for="file">Filename:</label>
+            <input type="file" name="file" id="file"><br>
+            <input type="submit" name="submit" value="Submit">
+        </form>
+
+    </div>
+</div><!-- container -->
+
+<!-- Footer -->
+<?php include('tpl/footer.php'); ?>
---
+++ public/less/skeleton/README.md
@@ -0,0 +1,12 @@
+This is the Pictura fork of Skeleton.
+
+Changelog
+=========
+
+1.0
+---
+* Moved stylesheets/ to components/
+* Renamed .css to .less
+* Split base.less into forms.less and buttons.less
+* Store media-query targets in variables so that they can be used like
+  `@media @phone { }`
