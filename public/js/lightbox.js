e9c4720057166e9f0f35cd6d0c4d71d0684b2fc8
Nguyen Quang Minh Hoang <minh@pictura.com.au>
2013-04-29 15:18:44 +1000

    Create a lightbox plugin

--- public/js/lightbox.js
+++ public/js/lightbox.js
@@ -1,32 +1,65 @@
-$(document).ready(function(){
-	var $createOverlay = function(){
-		$('<div class="lightbox-overlay"></div>').appendTo($("body"));
-		var $lightboxOverlayHeight = $(document).height();
-		$(".lightbox-overlay").css("height", $lightboxOverlayHeight);
-	};
+(function( $ ) {
+	var defaults = {
+        'overlayColor'          : 	'#000000',
+  		'overlayOpacity'		: 	'0.85', // value in between 0.00 and 1.00
+  		'effect'				: 	'fade', // options: fade, slide,
+  		'easing'				: 	'swing', // options: swing, linear
+  		'duration'				: 	'700',
+    };
+    var $_settings;
+ 	$.fn.lightbox = function(options, defaults) {
+  		
+  		var $settings = $.extend({}, defaults, options);
+  		$_settings = $settings;
+  		var $createOverlay = function(){
+			$('<div class="lightbox-overlay"></div>').appendTo($("body"));
+			var $lightboxOverlayHeight = $(document).height();
+			$(".lightbox-overlay").css({
+				'height'			: 	$lightboxOverlayHeight,
+				'background-color'	: 	$settings.overlayColor,
+				'opacity'			: 	$settings.overlayOpacity
+			});
+		};
 
-	var $showImage = function($imagePath){
-		var $image = '<div class="lightbox-image-container"><img src="'+ $imagePath +'"/></div>'; 
-		$($image).hide().appendTo($("body")).fadeIn(700,"swing");
-	}; 
+		var $showImage = function($imagePath){
+			var $image = '<div class="lightbox-image-container"><img src="'+ $imagePath +'"/></div>'; 
+			var $imageAppended = $($image).hide().appendTo($("body"));
+			
+			if ($settings.effect != 'slide'){
+				$imageAppended.fadeIn($settings.duration, $settings.easing);
+			}
+			if ($settings.effect == 'slide'){
+				$imageAppended.slideDown($settings.duration, $settings.easing);
+			}
+		}; 
 
-	var $closePopUp = function(){
-		var $closeButton = '<button class="close-button">Close</button>';
-		$($closeButton).appendTo(".lightbox-image-container");
-	};
+		var $showClosePopUpButton = function(){
+			var $closeButton = '<button class="close-button">Close</button>';
+			$($closeButton).appendTo(".lightbox-image-container");
+		};
 
-	$("a[rel=lightbox]").click(function(e){
-		var $imagePath = $(this).attr("href");
-		$createOverlay();	
-		$showImage($imagePath);
-		$closePopUp();
-		e.preventDefault();
+  		return this.click( function(e) {        
+			var $imagePath = $(this).attr("href");
+			$createOverlay();	
+			$showImage($imagePath);
+			$showClosePopUpButton();
+			e.preventDefault();
+	    });
+
+  		
+  	};
+  	$(document).on("click", ".close-button", function(){
+  		var $imageContainer = $(".lightbox-overlay, .lightbox-image-container");
+		if ($_settings.effect != 'slide'){
+			$imageContainer.fadeOut($_settings.duration, $_settings.easing, function() {
+				$(this).remove();	
+			});
+		}
+		if ($_settings.effect == 'slide'){
+			$imageContainer.slideUp($_settings.duration, $_settings.easing, function() {
+				$(this).remove();	
+			});
+		}
 	});
 
-	$(document).on("click", ".close-button", function(){
-		$(".lightbox-overlay, .lightbox-image-container").fadeOut(600, function() {
-			$(this).remove();	
-		});
-		
-	});
-});
+})( jQuery );
--- public/tpl/footer.php
+++ public/tpl/footer.php
@@ -2,6 +2,14 @@
 
 </footer>
 <script src="js/jquery.1.9.1.js"></script>
+
 <script src="js/lightbox.js"></script>
+<script type="text/javascript">
+	$("a[rel=lightbox]").lightbox({
+		'overlayColor'		:	'#cccccc',
+		'effect'			:	'fade',
+		'overlayOpacity'	: 	'0.8'
+	});
+</script>
 </body>
 </html>
