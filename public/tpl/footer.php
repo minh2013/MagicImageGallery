60171bb03348e8e906fad4dcb9e07f05b912a7fc
Nguyen Quang Minh Hoang <minh@pictura.com.au>
2013-05-01 22:36:00 +1000

    Change footer, create new CSS and JS files

--- public/tpl/footer.php
+++ public/tpl/footer.php
@@ -1,9 +1,7 @@
 <footer>
 
 </footer>
-<script src="js/jquery.1.9.1.js"></script>
-
-<script src="js/lightbox.js"></script>
+<script src="js/gallery.js"></script>
 <script type="text/javascript">
 	$("a[rel=lightbox]").lightbox({
 		'overlayColor'		:	'#cccccc',
