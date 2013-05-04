9bb8d52a81151f3bd6d9fb8f1c869e67b3c657d2
Nguyen Quang Minh Hoang <minh@pictura.com.au>
2013-05-04 13:04:15 +1000

    Create a configuration file

---
+++ public/configuration.php
@@ -0,0 +1,9 @@
+<?php
+	define('LARGE_IMAGE_PATH', 'uploads/large/');
+	define('THUMBNAIL_IMAGE_PATH', 'uploads/thumbnail/');
+	define('ORIGINAL_IMAGE_PATH', 'uploads/original/');
+	define('UPLOAD_MAX_FILE_SIZE', ini_get('upload_max_filesize'));
+	define('LARGE_IMAGE_WIDTH', 800);
+	define('LARGE_IMAGE_HEIGHT', 600);
+	define('THUMBNAIL_WIDTH', 300);
+	define('THUMBNAIL_HEIGHT', 275);
